5f87e54679a6bac3df82d40a5eb926d88ab98293
Jakub <41695668+AzethMeron@users.noreply.github.com>
2024-08-20 00:31:23 +0200

    Update main.py

--- mnist_example/main.py
+++ mnist_example/main.py
@@ -2,6 +2,8 @@
 from VirtualDirectory import VirtualDirectory, PillowDataManager
 import os
 
+# This script loads all images from one directory and puts it into VirtualDirectory, building the hierarchy with subdirectories on its own.
+
 mnist = os.listdir("mnist_single_dir")
 data_manager = PillowDataManager()
 vd = VirtualDirectory(root="mnist_vd", data_manager=data_manager, verbose=True, min_subdir_num=10, load_to_memory=True)
