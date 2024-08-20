d1c939086f66aba8ffaf4b5dc8ad5737f962632a
Jakub <41695668+AzethMeron@users.noreply.github.com>
2024-08-20 16:18:45 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='VirtualDirectory',
-    version='1.2.2',
+    version='1.2.3',
     packages=find_packages(),
     install_requires=[
         'tqdm>=4.66.5',
